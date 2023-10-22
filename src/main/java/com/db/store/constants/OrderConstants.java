e03f57941b82bd8a67882a76c332597a3bdc056b
Stanislav Skorobogatov <stanislav.skorobogatov0@gmail.com>
2023-10-23 01:08:34 +0300

    Integrate Lombok in constants enums for code efficiency
    Implemented Lombok annotations in Constants Enums, allowing for more concise code and reducing duplication. Additionally, added values to Enums to further streamline usage and maintain code consistency.

--- src/main/java/com/db/store/constants/OrderConstants.java
+++ src/main/java/com/db/store/constants/OrderConstants.java
@@ -1,15 +1,12 @@
 package com.db.store.constants;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum OrderConstants {
     ORDER_NOT_FOUND("order.not.found"),;
 
     private final String message;
-
-    OrderConstants(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
