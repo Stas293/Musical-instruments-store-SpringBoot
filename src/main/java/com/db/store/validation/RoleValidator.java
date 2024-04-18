995a85727505dd50da2ab538ca10ccf8c46b88cd
Stanislav Skorobogatov <iskor29@gmail.com>
2024-04-19 01:13:43 +0300

    Enhance readability and maintainability of validators
    Refactored validator classes to improve code readability and maintainability. This includes restructuring validation logic for better organization and clarity, resulting in more understandable and easily maintainable code.

--- src/main/java/com/db/store/validation/RoleValidator.java
+++ src/main/java/com/db/store/validation/RoleValidator.java
@@ -1,36 +1,38 @@
 package com.db.store.validation;
 
-import com.db.store.model.Role;
-import com.db.store.service.interfaces.RoleServiceInterface;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.db.store.dto.RoleDTO;
+import com.db.store.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class RoleValidator implements Validator {
-    private final RoleServiceInterface roleService;
-
-    @Autowired
-    public RoleValidator(RoleServiceInterface roleService) {
-        this.roleService = roleService;
-    }
+    private final RoleRepository roleRepository;
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return Role.class.equals(clazz);
+        return RoleDTO.class.equals(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-        Role role = (Role) target;
+        RoleDTO role = (RoleDTO) target;
 
-        if (roleService.getRoleByName(role.getName()).isPresent()) {
-            errors.rejectValue("name", "name", "Role with this name already exists");
-        }
+        roleRepository.findByName(role.getName())
+                .ifPresent(role1 -> errors.rejectValue(
+                        "name",
+                        "Role with this name already exists",
+                        "validation.role.name.exists"));
 
-        if (roleService.getRoleByCode(role.getCode()).isPresent()) {
-            errors.rejectValue("code", "code", "Role with this code already exists");
-        }
+        roleRepository.findByCode(role.getCode())
+                .ifPresent(role1 -> errors.rejectValue(
+                        "code",
+                        "Role with this code already exists",
+                        "validation.role.code.exists"));
     }
 }
--- src/main/java/com/db/store/validation/StatusValidator.java
+++ src/main/java/com/db/store/validation/StatusValidator.java
@@ -1,20 +1,16 @@
 package com.db.store.validation;
 
 import com.db.store.model.Status;
-import com.db.store.service.interfaces.StatusServiceInterface;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.db.store.repository.StatusRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 @Component
+@RequiredArgsConstructor
 public class StatusValidator implements Validator {
-    private final StatusServiceInterface statusService;
-
-    @Autowired
-    public StatusValidator(StatusServiceInterface statusService) {
-        this.statusService = statusService;
-    }
+    private final StatusRepository statusRepository;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -24,11 +20,17 @@ public class StatusValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         Status status = (Status) target;
-        if (statusService.getStatusByName(status.getName()) != null) {
-            errors.rejectValue("name", "status.name", "validation.status.exists.name");
-        }
-        if (statusService.getStatusByCode(status.getCode()) != null) {
-            errors.rejectValue("code", "status.code", "validation.status.exists.code");
-        }
+
+        statusRepository.findByName(status.getName())
+                .ifPresent(status1 -> errors.rejectValue(
+                        "name",
+                        "Status with this name already exists",
+                        "validation.status.name.exists"));
+
+        statusRepository.findByCode(status.getCode())
+                .ifPresent(status1 -> errors.rejectValue(
+                        "code",
+                        "Status with this code already exists",
+                        "validation.status.code.exists"));
     }
 }
