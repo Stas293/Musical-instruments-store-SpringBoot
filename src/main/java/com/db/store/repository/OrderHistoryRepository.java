e5f38c7594ac069db47198b4b4ce6061373980cc
Stanislav Skorobogatov <iskor29@gmail.com>
2024-04-19 01:13:34 +0300

    Refactor repositories for cleaner code structure
    Removed unnecessary @Repository annotations and introduced new methods in repositories to facilitate streamlined communication with services. This refactor improves code readability and maintains a cleaner separation of concerns between data access and business logic.

--- src/main/java/com/db/store/repository/OrderHistoryRepository.java
+++ src/main/java/com/db/store/repository/OrderHistoryRepository.java
@@ -5,11 +5,9 @@ import com.db.store.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Long> {
     Optional<OrderHistory> findByUserAndId(User user, Long id);
 
--- src/main/java/com/db/store/repository/UserRepository.java
+++ src/main/java/com/db/store/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.db.store.repository;
 
 import com.db.store.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLogin(String login);
 
