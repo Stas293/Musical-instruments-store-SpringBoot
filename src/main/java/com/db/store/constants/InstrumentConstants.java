70fa1de01f7b8d07d1e3b6436abec0212def5f36
Stanislav Skorobogatov <iskor29@gmail.com>
2024-04-19 01:13:52 +0300

    Integrate Lombok in constants enums for code efficiency
    Implemented Lombok annotations in Constants Enums, allowing for more concise code and reducing duplication. Additionally, added values to Enums to further streamline usage and maintain code consistency.

--- src/main/java/com/db/store/constants/InstrumentConstants.java
+++ src/main/java/com/db/store/constants/InstrumentConstants.java
@@ -1,18 +1,16 @@
 package com.db.store.constants;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum InstrumentConstants {
     INSTRUMENT_NOT_FOUND("instrument.not.found"),
     INSTRUMENT_TITLE_EXISTS("instrument.title.exists"),
     INSTRUMENT_DESCRIPTION_EXISTS("instrument.description.exists"),
-    INSTRUMENT_HAS_ORDERS("instrument.has.orders");
+    INSTRUMENT_HAS_ORDERS("instrument.has.orders"),
+    INSTRUMENT_FIELD_EXISTS("instrument.%s.exists");
 
     private final String message;
-
-    InstrumentConstants(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
--- src/main/java/com/db/store/constants/RoleConstants.java
+++ src/main/java/com/db/store/constants/RoleConstants.java
@@ -1,17 +1,15 @@
 package com.db.store.constants;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum RoleConstants {
     ROLE_NOT_FOUND("role.not.found"),
     ROLE_NAME_EXISTS("role.name.exists"),
-    ROLE_CODE_EXISTS("role.code.exists");
+    ROLE_CODE_EXISTS("role.code.exists"),
+    ROLE_FIELD_EXISTS("role.%s.exists");
 
     private final String message;
-
-    RoleConstants(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
